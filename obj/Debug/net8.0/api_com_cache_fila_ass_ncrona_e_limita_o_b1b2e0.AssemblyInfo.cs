0377a07b90d8f26177bd11860f6b08657138d379
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 13:59:25 -0300

    chore: projeto console .NET 8
    intent: chore

--- obj/Debug/net8.0/api_com_cache_fila_ass_ncrona_e_limita_o_b1b2e0.AssemblyInfo.cs
+++ obj/Debug/net8.0/api_com_cache_fila_ass_ncrona_e_limita_o_b1b2e0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api_com_cache_fila_ass_ncrona_e_limita_o_b1b2e0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a581de250c13fe47361a4aaea48f45d170fe36f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a1338caeed5579faf684477b7a25075a61a3a7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api_com_cache_fila_ass_ncrona_e_limita_o_b1b2e0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api_com_cache_fila_ass_ncrona_e_limita_o_b1b2e0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
