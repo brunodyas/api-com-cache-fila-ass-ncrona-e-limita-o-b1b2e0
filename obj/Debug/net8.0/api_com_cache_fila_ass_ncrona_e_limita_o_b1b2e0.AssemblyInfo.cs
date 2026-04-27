7bed166cea3f3b2db3bdd93668e4f32cf361812d
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 13:58:23 -0300

    feat: entrada CLI mínima
    intent: feature

--- obj/Debug/net8.0/api_com_cache_fila_ass_ncrona_e_limita_o_b1b2e0.AssemblyInfo.cs
+++ obj/Debug/net8.0/api_com_cache_fila_ass_ncrona_e_limita_o_b1b2e0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api_com_cache_fila_ass_ncrona_e_limita_o_b1b2e0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75f47fbc8a40794e413281d212654c2618ccc5ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6f14b6198d8b973a70b31a743c9acfccf6ed614")]
 [assembly: System.Reflection.AssemblyProductAttribute("api_com_cache_fila_ass_ncrona_e_limita_o_b1b2e0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api_com_cache_fila_ass_ncrona_e_limita_o_b1b2e0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
